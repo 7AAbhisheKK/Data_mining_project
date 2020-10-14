14dff0b50bbcc1feec3a8f91d718efb3ab5317f9
7AAbhisheKK <ar23072000@gmail.com>
2020-10-15 05:21:34 +0530

    edits

--- layer.py
+++ layer.py
@@ -58,103 +58,39 @@ def linear_backward(dZ,cache):
 
     return dA_prev, dW, db
 def linear_activation_backward(dA, cache, activation):
-    """
-    Implement the backward propagation for the LINEAR->ACTIVATION layer.
-    
-    Arguments:
-    dA -- post-activation gradient for current layer l 
-    cache -- tuple of values (linear_cache, activation_cache) we store for computing backward propagation efficiently
-    activation -- the activation to be used in this layer, stored as a text string: "sigmoid" or "relu"
-    
-    Returns:
-    dA_prev -- Gradient of the cost with respect to the activation (of the previous layer l-1), same shape as A_prev
-    dW -- Gradient of the cost with respect to W (current layer l), same shape as W
-    db -- Gradient of the cost with respect to b (current layer l), same shape as b
-    """
     linear_cache, activation_cache = cache
     
     if activation == "relu":
-        ### START CODE HERE ### (≈ 2 lines of code)
         dZ = relu_backward(dA,activation_cache)
         dA_prev, dW, db = linear_backward(dZ, cache[0])
-        ### END CODE HERE ###
         
     elif activation == "sigmoid":
-        ### START CODE HERE ### (≈ 2 lines of code)
         dZ = sigmoid_backward(dA,activation_cache)
         dA_prev, dW, db = linear_backward(dZ, cache[0])
-        ### END CODE HERE ###
     
     return dA_prev, dW, db
 
 def L_model_backward(AL, Y, caches):
-    """
-    Implement the backward propagation for the [LINEAR->RELU] * (L-1) -> LINEAR -> SIGMOID group
-    
-    Arguments:
-    AL -- probability vector, output of the forward propagation (L_model_forward())
-    Y -- true "label" vector (containing 0 if non-cat, 1 if cat)
-    caches -- list of caches containing:
-                every cache of linear_activation_forward() with "relu" (it's caches[l], for l in range(L-1) i.e l = 0...L-2)
-                the cache of linear_activation_forward() with "sigmoid" (it's caches[L-1])
-    
-    Returns:
-    grads -- A dictionary with the gradients
-             grads["dA" + str(l)] = ... 
-             grads["dW" + str(l)] = ...
-             grads["db" + str(l)] = ... 
-    """
+   
     grads = {}
-    L = len(caches) # the number of layers
-    # m = AL.shape[1]
-    # Y = Y.reshape(AL.shape) # after this line, Y is the same shape as AL
-    
-    # Initializing the backpropagation
-    ### START CODE HERE ### (1 line of code)
+    L = len(caches) 
     dAL = softmax_grad(AL,Y)
-    ### END CODE HERE ###
-    
-    # Lth layer (SIGMOID -> LINEAR) gradients. Inputs: "dAL, current_cache". Outputs: "grads["dAL-1"], grads["dWL"], grads["dbL"]
-    ### START CODE HERE ### (approx. 2 lines)
     current_cache = caches[L-1]
     grads["dA" + str(L-1)], grads["dW" + str(L)], grads["db" + str(L)] = linear_activation_backward(dAL, current_cache,"relu")
-    ### END CODE HERE ###
-    
-    # Loop from l=L-2 to l=0
     for l in reversed(range(L-1)):
-        # lth layer: (RELU -> LINEAR) gradients.
-        # Inputs: "grads["dA" + str(l + 1)], current_cache". Outputs: "grads["dA" + str(l)] , grads["dW" + str(l + 1)] , grads["db" + str(l + 1)] 
-        ### START CODE HERE ### (approx. 5 lines)
         current_cache = caches[l]
         dA_prev_temp, dW_temp, db_temp = linear_activation_backward(grads["dA"+str(l+1)], current_cache,"relu")
         grads["dA" + str(l)] = dA_prev_temp
         grads["dW" + str(l + 1)] = dW_temp
         grads["db" + str(l + 1)] = db_temp
-        ### END CODE HERE ###
 
     return grads
 def update_parameters(parameters, grads, learning_rate):
-    """
-    Update parameters using gradient descent
     
-    Arguments:
-    parameters -- python dictionary containing your parameters 
-    grads -- python dictionary containing your gradients, output of L_model_backward
-    
-    Returns:
-    parameters -- python dictionary containing your updated parameters 
-                  parameters["W" + str(l)] = ... 
-                  parameters["b" + str(l)] = ...
-    """
-    
-    L = len(parameters) // 2 # number of layers in the neural network
-
-    # Update rule for each parameter. Use a for loop.
-    ### START CODE HERE ### (≈ 3 lines of code)
+    L = len(parameters) // 2 
     for l in range(L):
         parameters["W" + str(l+1)] =parameters["W" + str(l+1)]-learning_rate*grads["dW"+str(l+1)]
         parameters["b" + str(l+1)] =parameters["b" + str(l+1)]-learning_rate*grads["db"+str(l+1)]
-    ### END CODE HERE ###
     return parameters            
 
 
